f59ec337859ee6f00255aae7e52b29791cf31d1b
Eugen Fazekas <skybolt83@gmail.com>
2021-06-25 18:10:23 +0300

    added Authentication Service to Practice_02

---
+++ SpringBoot_MicroService_Practice_02/OAuth2_JWT_Resource_Server/src/main/java/com/OAuth2JwtResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2JwtResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2JwtResourceServerApplication.class, args);
+	}
+
+}
