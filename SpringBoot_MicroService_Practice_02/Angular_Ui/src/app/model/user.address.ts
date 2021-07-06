983cad80e14cb0985aeeb5ec917fa81f145768da
Eugen Fazekas <skybolt83@gmail.com>
2021-07-06 22:28:41 +0300

    Practice01 & Practice_02 moved to SpringBoot Microservices MyProjects gitRepo

--- SpringBoot_MicroService_Practice_02/Angular_Ui/src/app/model/user.address.ts
+++
@@ -1,9 +0,0 @@
-export class UserAddress {
- 				 
-	constructor(
-                 public country?: string,
-				 public city?: string,
-				 public street?: string,
-				 public number?: string
-				) {}
-}
