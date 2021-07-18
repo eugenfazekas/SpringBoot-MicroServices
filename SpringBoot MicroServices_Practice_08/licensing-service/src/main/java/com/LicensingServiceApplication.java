cef0521f42dcab3f7fdad527fe75cc4ca766f29e
Eugen Fazekas <skybolt83@gmail.com>
2021-07-18 19:39:19 +0300

    init Event-driven architecture

---
+++ SpringBoot MicroServices_Practice_08/licensing-service/src/main/java/com/LicensingServiceApplication.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import com.utils.UserContextInterceptor;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class LicensingServiceApplication {
+
+  @LoadBalanced
+  @Bean
+  public RestTemplate getRestTemplate(){
+	  RestTemplate template = new RestTemplate();
+      List interceptors = template.getInterceptors();
+      if (interceptors==null){
+          template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+      }
+      else{
+          interceptors.add(new UserContextInterceptor());
+          template.setInterceptors(interceptors);
+      }
+
+      return template;
+  }
+
+  public static void main(String[] args) {
+        SpringApplication.run(LicensingServiceApplication.class, args);
+  }
+}
