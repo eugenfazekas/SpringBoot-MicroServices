461e9ce2c702d818c14f84a5dc5572bb4462acf3
Eugen Fazekas <skybolt83@gmail.com>
2021-07-25 17:39:43 +0300

    Practice_10 implementing zipkin-service

---
+++ SpringBoot MicroServices_Practice_10/licensing-service/src/main/java/com/repository/LicenseRepository.java
@@ -0,0 +1,15 @@
+package com.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.model.License;
+
+import java.util.List;
+
+@Repository
+public interface LicenseRepository extends CrudRepository<License,String>  {
+    public List<License> findByOrganizationId(String organizationId);
+    public License findByOrganizationIdAndLicenseId(String organizationId,String licenseId);
+}
